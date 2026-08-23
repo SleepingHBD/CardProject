7b8941233e25f2af433b3a7fcc39c81adacdef64
SleepingHBD <dinieazfie03@gmail.com>
2026-08-23 14:18:15 +0800

    Remove Pet Expedition mode

--- test/main-menu.test.js
+++ test/main-menu.test.js
@@ -11,7 +11,6 @@ test("the main menu excludes the retired World Chronicle", () => {
 
 test("the main menu retains its playable navigation", () => {
   assert.match(pageSource, /id="mainMenuPlayButton"/);
-  assert.match(pageSource, /id="mainMenuExpeditionButton"/);
   assert.match(pageSource, /id="mainMenuTutorialButton"/);
   assert.match(pageSource, /id="mainMenuRulebookButton"/);
   assert.match(pageSource, /id="mainMenuSettingsButton"/);
